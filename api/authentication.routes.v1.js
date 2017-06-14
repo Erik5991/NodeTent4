2b045908c741e0cef5751c6f949a95a72dddb6ef
teun2408 <teun2408@gmail.com>
2017-06-14 12:07:04 +0200

    Revert "Feature/login controlle"

--- api/authentication.routes.v1.js
+++ api/authentication.routes.v1.js
@@ -1,3 +1,9 @@
+//
+// ./api/authentication.routes.v1.js
+//
+//
+// ./api/routes_v2.js
+//
 var express = require('express');
 var router = express.Router();
 var db = require('../config/db');
@@ -6,27 +12,23 @@ var auth = require('../auth/authentication');
 router.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
+    var dbemail;
 
     if(username != '' || password != ''){
-        db.query("SELECT password FROM customer WHERE email = '" + username + "'", function (error, result, fields) {
-            if (error != null) {
+        db.query("SELECT COUNT(*) AS count FROM customer WHERE email = '" + username + "' AND password = '" + password + "'", function (error, result) {
+            if (error) {
                 res.status(400).json(error);
             } else {
-                if (result.length !== null) {
-                    if (result[0].password == password) {
-                        var token = auth.encodeToken(username);
-                        res.status(200).json({
-                            "token": token
-                        });
-                    }
-                    else {
-                        res.status(401).json({
-                            "error": "verkeerde gegevens"
-                        })
-                    }
+                if(result[0].count >= 1){
+                    var token = auth.encodeToken(username);
+                    res.status(200).json({
+                        "token": token
+                    });
                 }
                 else {
-                    res.status(200).json(result)
+                    res.status(401).json({
+                        "error": "verkeerde gegevens"
+                    })
                 }
             }
         })
