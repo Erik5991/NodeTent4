44510b64a872f46316ac552d74240f4b360e5c07
Teun van Zon <teun2408@gmail.com>
2017-06-15 14:27:16 +0200

    de rental calls aangepast en zijn allemaal werkend

--- api/rental.routes.v1.js
+++ api/rental.routes.v1.js
@@ -2,84 +2,57 @@ var express = require('express');
 var routes = express.Router();
 var db = require('../config/db');
 
-routes.get('/rentals/:id', function(req, res) {
+routes.get('/rentals/:userid', function(req, res) {
+    var userID = req.params.userid;
 
-    var rentalID = req.params.id;
-
-    res.contentType('application/json');
-
-    db.query('SELECT * FROM rental WHERE rental_id =?', [ rentalID ], function(error, rows, fields) {
+    db.query('SELECT * FROM rental WHERE customer_id =?', [ userID ], function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
             res.status(200).json({ result: rows });
-        };
+        }
     });
 });
 
-routes.post('/rentals/insert', function(req, res) {
-
-
-    var query = {
-        sql: 'INSERT INTO `rental` (`rental_id`, `rental_date`, `inventory_id`, `customer_id`, `return_date`) VALUES (NULL, CURRENT_TIMESTAMP, 2, 2, NULL)',
-
-        timeout: 2000 // 2secs
-    };
-
-    console.log('Onze query: ' + query.sql);
+routes.post('/rentals/:userid/:inventoryid', function(req, res) {
+    var userID = req.params.userid;
+    var inventoryID = req.params.inventoryid;
 
     res.contentType('application/json');
-    db.query(query, function(error, rows, fields) {
+    db.query('INSERT INTO `rental` (`rental_id`, `rental_date`, `inventory_id`, `customer_id`, `return_date`) VALUES (NULL, CURRENT_TIMESTAMP, ' + inventoryID +  ', ' + userID +  ', NULL)', function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
             res.status(200).json({ result: rows });
-        };
+        }
     });
 });
 
-routes.put('/rentals/change/:id/:invID', function(req, res) {
-
-    var todos = req.body;
-    var customerId = req.params.id;
-    var inventoryId = req.params.invID;
-    var query = {
-        sql: "UPDATE `rental`  ()",
-        values: [todos.Title, todos.Beschrijving, ID],
-        timeout: 2000 // 2secs
-    };
-
-    console.log('Onze query: ' + query.sql);
+routes.put('/rentals/:userid/:inventoryid', function(req, res) {
+    var customerId = req.params.userid;
+    var inventoryId = req.params.inventoryid;
 
     res.contentType('application/json');
-    db.query(query, function(error, rows, fields) {
+    db.query('UPDATE rental SET return_date = CURRENT_TIMESTAMP WHERE customer_id = ' + customerId +' AND inventory_id = ' + inventoryId, function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
             res.status(200).json({ result: rows });
-        };
+        }
     });
 });
 
-
-routes.delete('/rentals/delete/:id', function(req, res) {
-
-    var ID = req.params.id;
-    var query = {
-        sql: 'DELETE FROM `rental` WHERE inventory_id=?',
-        values: [ID],
-        timeout: 2000 // 2secs
-    };
-
-    console.log('Onze query: ' + query.sql);
+routes.delete('/rentals/:userid/:inventoryid', function(req, res) {
+    var customerId = req.params.userid;
+    var inventoryId = req.params.inventoryid;
 
     res.contentType('application/json');
-    db.query(query, function(error, rows, fields) {
+    db.query('DELETE FROM rental WHERE customer_id = ' + customerId +' AND inventory_id = ' + inventoryId, function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
             res.status(200).json({ result: rows });
-        };
+        }
     });
 });
 
